54dcc1e595be65324eba0224b63d63382c465365
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-10-29 11:29:05 -0400

    Create Coleccion de Numeros.cpp

---
+++ II-2021/ESTRUCTURA DE DATOS/Coleccion de Numeros.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    char ope;
+    int valor;
+
+    // heap max
+    priority_queue<int> q;
+    while(cin >> ope && ope != 'T'){
+        if(ope == 'S'){
+            cin >> valor;
+            q.push(valor);
+        }else if(ope == 'I' or ope == 'D'){
+            cin >> valor;
+            int mayor = q.top();
+            if(ope == 'D')
+                valor *= -1;
+            mayor += valor;
+            q.pop();
+            q.push(mayor);
+        }else if(ope == 'A'){
+            if(q.size() > 0)
+                cout << q.top() << '\n';
+            else
+                cout << "Error\n";
+        }else if(ope == 'R'){
+            if(q.size() > 0)
+                q.pop();
+            else
+                cout << "Error\n";
+        }
+    }
+
+    return 0;
+}
