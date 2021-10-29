d9370a44124c9a7f73ccb7abf694c7c4e9e08377
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-10-29 11:02:54 -0400

    Create Banco Vitalicio.cpp

---
+++ II-2021/ESTRUCTURA DE DATOS/Banco Vitalicio.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // c++ heap (cola de prioridad) es de maximos
+    int n, m, p;
+
+    cin >> n >> m >> p;
+    priority_queue< pair<int, char> > q;
+
+    while(n--){
+        int t;
+        char op;
+        cin >> t >> op;
+        q.push({ -t , op });
+    }
+
+    int tiempo = -q.top().first;
+
+    while(not q.empty()){
+        tiempo += p;
+        if(tiempo <= m)
+            cout << tiempo << '\n';
+        else
+            cout << "Mil disculpas, regrese mañana\n";
+        
+        q.pop();
+    }
+
+    return 0;
+}
