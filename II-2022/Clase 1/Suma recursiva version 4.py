930a821fefa4e85de96d590aa636303a32df416d
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2022-08-15 19:47:05 -0400

    Create Suma recursiva version 4.py

---
+++ II-2022/Clase 1/Suma recursiva version 4.py
@@ -0,0 +1,73 @@
+import sys
+ 
+ 
+ 
+def power_recursivo(a, b):
+    if b == 0:
+        return 1
+     
+    ans = power_recursivo(a * a, b // 2);
+    if b % 2 == 1:
+        ans *= a
+     
+    return ans
+     
+     
+def power_lento(a, b):
+    ans = 1
+    for i in range(1, b + 1):
+        ans = ans * a
+     
+    return ans
+ 
+def power(a, b):
+    ans = 1
+    while b > 0:
+         
+        if b % 2 == 1:
+            ans *= a
+             
+        a *= a
+        b //= 2
+ 
+    return ans
+ 
+def f(x):
+    if x == 0:
+        return 0
+    return f(x // 10) + x % 10
+     
+# x = input()
+ 
+def suma(x):
+    ans = 0
+    while x > 0:
+        ans += x % 10
+        x //= 10
+    return ans
+ 
+ 
+ 
+a = 1
+b = 99123232343434
+ 
+# print(power(a, b))
+# print(power_lento(a, b))
+ 
+ 
+# print(power(a, b))
+# print(power_recursivo(a, b))
+ 
+# principal  (da AC)
+t = int(sys.stdin.readline())
+for i in range(t):
+    x, k = map(int, sys.stdin.readline().split())
+    ans = power(x, k)
+     
+     
+    # vamos a sumar mientras ans > 10
+    ans = ans % 9
+    if ans == 0:
+        ans = 9
+    # print(ans)
+    sys.stdout.write( str(ans) + "\n")
