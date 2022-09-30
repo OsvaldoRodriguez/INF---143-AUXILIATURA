c401f026cc71ea50ea2f608618b91e555a3fe3f1
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2022-09-30 14:14:20 -0400

    Create stack.py

---
+++ II-2022/Estructura de datos/stack.py
@@ -0,0 +1,17 @@
+pila = []
+# append() es O(1)
+pila.append(5)  # [5]      
+pila.append(7)  # [5, 7]
+pila.append(9)  # [5, 7, 9]
+print('Pila_inicial') 
+print(pila) 
+  
+# pop() para obtener el ultimo elemento 
+# print(’Elementos_extraidos_de_la_pila:’) 
+print(pila.pop()) 
+print(pila.pop())
+ 
+ 
+# para el tamanio es len(nombre)
+ 
+print(len(pila))
